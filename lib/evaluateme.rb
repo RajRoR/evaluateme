c57bd25b5c35a5082c6aa71331d649530fdd52c6
Sunia kalra <skalra17@yahoo.in>
2015-04-16 13:31:00 +0530

    Initializer file of the plugin.

--- lib/evaluateme.rb
+++ lib/evaluateme.rb
@@ -1,4 +1,21 @@
+# Initializer file of the plugin.
+
 require "evaluateme/engine"
 
 module Evaluateme
+
+  # Secret key.
+  mattr_accessor :secret_key
+  @@secret_key = "evaluateme321"
+
+  mattr_accessor :parent_controller
+  @@parent_controller = "ApplicationController"
+
+  mattr_accessor :mailer_sender
+  @@mailer_sender = 'admin@evaluateme.com' 
+
+  def self.setup
+    yield self
+  end
+
 end
