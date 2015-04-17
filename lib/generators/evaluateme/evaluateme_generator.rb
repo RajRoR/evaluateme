bbdf7dfb0d3f1fa0c91572db17ea007d75c53c24
Sunia kalra <skalra17@yahoo.in>
2015-04-17 18:32:26 +0530

    Migration file to add new fields in the hooked application.

--- lib/generators/evaluateme/evaluateme_generator.rb
+++ lib/generators/evaluateme/evaluateme_generator.rb
@@ -6,17 +6,37 @@ class EvaluatemeGenerator < Rails::Generators::NamedBase
 # Copy the helper file to the application.
   def copy_helper_file
     create_file "app/helpers/evaluateme_helper.rb", 
-
 <<-FILE
 
 module EvaluatemeHelper
-
-  def #{plural_name.singularize}_plugin_helper?
+  
+  def #{plural_name.singularize}_plugin_helper
     puts "Evaluateme plugin."
   end
 
 end
 FILE
-
+    puts "Helper file has created"
   end
+
+
+# Create the migrations
+  def create_user_migration
+    plural_name = 'user' if plural_name.blank? 
+
+    create_file "db/migrate/add_evaluateme_fields_to_#{plural_name}",
+
+<<-MIGRATION
+class AddEvaluatemeTo#{plural_name.capitalize} < ActiveRecord::Migration
+  def change
+    
+    add_column :#{plural_name}, :kra_locked, :boolean, :default => false
+    add_column :#{plural_name}, :kra, :boolean, :default => false
+  end
+end
+
+MIGRATION
+  puts "Migration has created"
+end
+
 end
